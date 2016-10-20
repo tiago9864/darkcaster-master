6afc82bcc1319b57cde7f885bf6b84bb060f1337
tiago9864 <santiago2091@gmail.com>
2016-10-19 20:03:38 -0400

    Added new app

---
+++ routers/forecast.router.js
@@ -0,0 +1,27 @@
+
+
+var express = require('express');
+var router = express.Router();
+var apiKey = process.env.APIKEY || require('../config.js').apiKey;
+var axios = require('axios');
+var timeoutConfig = {
+  timeout: 2000
+};
+router.get('/forecast/:latitude,:longitude', function(request, response){
+  var url = buildForecastURL(request.params.latitude, request.params.longitude);
+  axios.get(url, timeoutConfig)
+        .then(function(forecast){
+          response.send(forecast.data);
+        })
+        .catch(function(error){
+          response.send(error);
+        });
+});
+
+module.exports = router;
+
+function buildForecastURL(latitude, longitude){
+  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'
+  + latitude +','+ longitude;
+  return url;
+}
