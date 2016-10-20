54e10acec30ee03f4aa9ee9306f4f15e901cca01
tiago9864 <santiago2091@gmail.com>
2016-10-19 20:17:31 -0400

    catching up

--- routers/forecast.router.js
+++ routers/forecast.router.js
@@ -4,9 +4,11 @@ var express = require('express');
 var router = express.Router();
 var apiKey = process.env.APIKEY || require('../config.js').apiKey;
 var axios = require('axios');
+var authorize = require('../middleware/auth.js');
 var timeoutConfig = {
   timeout: 2000
 };
+router.use(authorize)
 router.get('/forecast/:latitude,:longitude', function(request, response){
   var url = buildForecastURL(request.params.latitude, request.params.longitude);
   axios.get(url, timeoutConfig)
